9055cc0c2bb0c5e833fd5261c38d1227becdb6fe
Xidiantongwen <13275718528@163.com>
2020-02-21 22:07:57 +0800

    创建login

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
 	return Httpresponse('hello world')
+
+
+def login(request):
+    return redirect('www.baidu.com')
